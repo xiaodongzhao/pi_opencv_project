7d3ce309729bbec177baf284532eed55e397c29d
Xiaodong Zhao <xiaodong.zhao@warwick.ac.uk>
2019-04-29 11:59:24 +0100

    slide update

--- scripts/download_weights.sh
+++ scripts/download_weights.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-wget https://pjreddie.com/media/files/yolov3.weights
+wget https://pjreddie.com/media/files/yolov3.weights -P ../yolo_coco
+wget https://pjreddie.com/media/files/yolov3-tiny.weights -P ../yolo_coco
+
